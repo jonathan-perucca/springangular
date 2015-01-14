a1c4cdef59c30d5b407b33d762438f74434877e0
jperucca <under.64@gmail.com>
2015-01-14 15:08:52 +0100

    Fastern application context scan

--- src/main/java/springangular/SpringangularApplication.java
+++ src/main/java/springangular/SpringangularApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan({"springangular.web", "springangular.repository"})
 @EnableAutoConfiguration
 public class SpringangularApplication {
 

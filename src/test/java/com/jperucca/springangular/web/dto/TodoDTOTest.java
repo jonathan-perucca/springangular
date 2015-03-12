f0eed99f4db5bdfcaf9e504633a058b3d12f6543
jperucca <under.64@gmail.com>
2015-03-13 00:09:36 +0100

    [NEW] added demo for Pojo to DTO mapping with dozer mapper made very simply

--- src/test/java/com/jperucca/springangular/web/dto/TodoDTOTest.java
+++ src/test/java/com/jperucca/springangular/web/dto/TodoDTOTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.jperucca.springangular.domain.Todo.newTodo;
+import static com.jperucca.springangular.web.dto.TodoDTO.newTodoDTO;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -35,5 +36,20 @@ public class TodoDTOTest {
         assertThat(todoDTO.getId(), is(id));
         assertThat(todoDTO.isChecked(), is(checked));
         assertThat(todoDTO.getDescription(), is(description));
+    }
+
+    @Test
+    public void should_SerializePojoToDTO_OnTodo() {
+        long id = 1L;
+        boolean checked = true;
+        String description = "descr";
+        TodoDTO todoDTO = newTodoDTO().withId(id).withChecked(checked).withDescription(description).build();
+
+        Todo todo = mapper.map(todoDTO, Todo.class);
+
+        assertThat(todo, notNullValue());
+        assertThat(todo.getId(), is(id));
+        assertThat(todo.isChecked(), is(checked));
+        assertThat(todo.getDescription(), is(description));
     }
 }
